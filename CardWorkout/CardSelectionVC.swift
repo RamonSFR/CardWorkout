f91f7678a5b1c70e0c425863025806458e408398
RamonSFR <ramonsaviofontesrocha@gmail.com>
2025-09-26 15:47:25 -0300

    programmatic UI initial project setup

--- CardWorkout/CardSelectionVC.swift
+++ CardWorkout/CardSelectionVC.swift
@@ -2,53 +2,15 @@
 //  CardSelectionVC.swift
 //  CardWorkout
 //
-//  Created by Ramon Sávio Fontes Rocha on 17/09/25.
+//  Created by Ramon Sávio Fontes Rocha on 26/09/25.
 //
 
 import UIKit
 
 class CardSelectionVC: UIViewController {
 
-    @IBOutlet var cardImageView: UIImageView!
-    @IBOutlet var buttons: [UIButton]!
-    
-    var cards: [UIImage] = Card.allValues
-    
-    var timer: Timer!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        startTimer()
-        
-        for button in buttons {
-            button.layer.cornerRadius = 8
-        }
-    }
-    
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        timer.invalidate()
-    }
-    
-    func startTimer() {
-        timer = Timer.scheduledTimer(timeInterval: 0.15,
-                                     target: self,
-                                     selector: #selector(showRandomImage),
-                                     userInfo: nil,
-                                     repeats: true)
-    }
-    
-    @objc func showRandomImage() {
-        cardImageView.image = cards.randomElement() ?? UIImage(named: "AS")
-    }
-
-    @IBAction func stopButtonTapped(_ sender: UIButton) {
-        timer.invalidate()
-    }
-    
-    
-    @IBAction func restartButtonTapped(_ sender: UIButton) {
-        timer.invalidate()
-        startTimer()
+        view.backgroundColor = .brown
     }
 }
